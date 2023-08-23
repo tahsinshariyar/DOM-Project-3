a048230eb1309c7fc5c2a40b2318d9f4b4989e98
Tahsin Shariyar <120914832+tahsinshariyar@users.noreply.github.com>
2023-08-23 13:06:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accordian = document.getElementsByClassName('content-container');
+
+    for (let i = 0; i < accordian.length; i++) {
+        accordian[i].addEventListener('click', function () {
+            this.classList.toggle('active');
+        });
+    }
+});
